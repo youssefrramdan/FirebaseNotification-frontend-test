9121a5f6a6e33470a0b0ee1fd5a59b3e865a2510
youssefrramdan <youfahmed353@gmail.com>
2025-05-02 14:23:30 +0300

    Remove unused assets and testing dependencies, and clean up package configuration

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
@@ -11,9 +10,6 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// Start performance measurement
-reportWebVitals();
 
 // ✅ Register Firebase Messaging Service Worker
 if ("serviceWorker" in navigator) {
